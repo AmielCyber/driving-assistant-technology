0bab9b55d17b74c301b2a34c86b165078c7ee28a
Amiel Nava <amieln@icloud.com>
2026-07-05 15:16:38 -0700

    Create interface for all ADAS features

---
+++ src/ADASFeature.h
@@ -0,0 +1,25 @@
+#ifndef ADAS_FEATURE_H
+#define ADAS_FEATURE_H
+#include <opencv2/core/mat.hpp>
+
+/**
+ * ADAS Feature that each our feature will implement in order
+ * to add feature annotations to the passed frame.
+ */
+class ADASFeature {
+public:
+  virtual ~ADASFeature() = default;
+  /**
+   * Process a frame for ADAS features and returns an annotated frame based
+   * on the feature.
+   * @param frame The frame to analyze the features
+   * @return A frame that will display the features of the passed frame
+   */
+  virtual cv::Mat process(cv::Mat frame) = 0;
+  /**
+   * Returns the name for this feature. Can be used to name a window
+   * @return Returns the name for this feature
+   */
+  virtual std::string get_feature_name() = 0;
+};
+#endif // ADAS_FEATURE_H
