7ec2ff011cace8e0bf7e1c00923a71cfd9c3b80b
Carlos Cazares <cgcazares1@csuchico.edu>
2026-07-19 11:37:34 -0700

    Add files via upload
    Replacing using ONNX instead of OpenCV

--- src/Object-Detection/YOLOVideoDetector.cpp
+++ src/Object-Detection/YOLOVideoDetector.cpp
@@ -4,16 +4,50 @@
 #include <iostream>
 #include <iomanip>
 
+#ifdef __APPLE__
+#include <coreml_provider_factory.h>
+#endif
+
 using namespace cv;
-using namespace cv::dnn;
 using namespace std;
 
 // Constructor using parameters
-YOLOVideoDetector::YOLOVideoDetector(const string& modelPath,
-                                     const string& classFilePath)
+YOLOVideoDetector::YOLOVideoDetector(
+        const string& modelPath,
+        const string& classFilePath)
+    :
+      env(ORT_LOGGING_LEVEL_WARNING, "YOLO"),
+      memoryInfo(
+          Ort::MemoryInfo::CreateCpu(
+              OrtArenaAllocator,
+              OrtMemTypeDefault))
 {
     loadClasses(classFilePath);
     loadModel(modelPath);
+
+    // Open the log
+    logFile.open("detections.csv");
+    // This is the structure
+    if (logFile.is_open())
+    {
+        logFile << "frame,class,confidence,"
+                << "center_x,center_y,"
+                << "left,top,width,height,"
+                << "frame_width,frame_height\n";
+    }
+    else // In case the log cannot be created
+    {
+        cerr << "Cannot open detections.csv for writing.\n";
+    }
+}
+
+// Destructor to close the log at the end of the program
+YOLOVideoDetector::~YOLOVideoDetector()
+{
+    if (logFile.is_open())
+    {
+        logFile.close();
+    }
 }
 
 // Helper to load the classes names used in the detection Default is classes.names
@@ -38,20 +72,70 @@ void YOLOVideoDetector::loadClasses(const string& filename)
 // Helper to load the YOLO trained Model used in the objects detection
 void YOLOVideoDetector::loadModel(const string& modelPath)
 {
-    net = readNet(modelPath);
-    net.setPreferableBackend(DNN_BACKEND_OPENCV);
-    net.setPreferableTarget(DNN_TARGET_CPU);
+    // This provides flexibility to try new trained models
+    sessionOptions.SetGraphOptimizationLevel(
+        GraphOptimizationLevel::ORT_ENABLE_ALL);
+
+
+    // Enable Apple Neural Engine / GPU through CoreML
+#ifdef __APPLE__   
+    Ort::ThrowOnError(
+	OrtSessionOptionsAppendExecutionProvider_CoreML(
+            sessionOptions,
+            0));
+#endif    
+    session =
+        std::make_unique<Ort::Session>(
+            env,
+            modelPath.c_str(),
+            sessionOptions);
+    
+    // To understand how what is doing the process
+    /*size_t providerCount = 0;
+
+        auto providers =
+            Ort::GetAvailableProviders();
+
+        for (const auto& provider : providers)
+        {
+            std::cout << "Available provider: "
+                      << provider
+                      << std::endl;
+        }*/
+    
+    Ort::AllocatorWithDefaultOptions allocator;
+
+    // Input
+    auto inputName =
+        session->GetInputNameAllocated(0, allocator);
+
+    inputNameStorage.push_back(inputName.get());
+    inputNames.push_back(inputNameStorage.back().c_str());
+
+    // Output
+    auto outputName =
+        session->GetOutputNameAllocated(0, allocator);
+
+    outputNameStorage.push_back(outputName.get());
+    outputNames.push_back(outputNameStorage.back().c_str());
 }
 
-// Main method to detect objects
-cv::Mat YOLOVideoDetector::process(cv::Mat& frame)
+// Main method to detect objects, receive a frame and provide a processed frame
+// This is our pipe line design
+cv::Mat YOLOVideoDetector::process(cv::Mat &frame)
 {
-    std::vector<int> classIds;
-    std::vector<float> confidences;
-    std::vector<cv::Rect> boxes;
+    // Variables for the detected objects and highlight them in the frame
+    std::vector<int> classIds; // Vector to store the detected classes
+    std::vector<float> confidences; // Vector to store the detected confidence rates
+    std::vector<cv::Rect> boxes; // Vector to store the boxes for each detected object
 
+    // Temp Timer
+    int64 frameStart = cv::getTickCount();
+    
+    // Launch the detection method and send the frame and recieve all the detected object info.
     detectObjects(frame, classIds, confidences, boxes);
 
+    // sequence of the detected objects from the neural network
     std::vector<int> indices;
 
     cv::dnn::NMSBoxes(
@@ -62,17 +146,42 @@ cv::Mat YOLOVideoDetector::process(cv::Mat& frame)
         indices
     );
 
+    // for all detected object, create a box with level and confidence score in the output frame
     for (int idx : indices)
     {
         cv::Rect box = boxes[idx];
         std::string className = getClassName(classIds[idx]);
+        
+        // Write a record for each of the detected objects in the log
+        logDetection(
+            frameNumber, // Num of frame
+            className, // Class
+            confidences[idx], // Confidence of the detection
+            box, // Box of the detected object
+            frame.size() // Size of the frame
+        );
 
         drawDetection(frame, box, className, confidences[idx]);
     }
 
+    frameNumber++;   // next frame
+    
+    // Timer
+    double frameTime =
+        (cv::getTickCount() - frameStart) /
+        cv::getTickFrequency();
+
+    std::cout
+        << "Frame: "
+        << frameTime * 1000
+        << " ms   FPS: "
+        << 1.0/frameTime
+        << "\n";
+    
     return frame;
 }
 
+// Control for the main cpp to identify what feature is performing
 std::string YOLOVideoDetector::get_feature_name()
 {
     return "objects";
@@ -84,32 +193,132 @@ void YOLOVideoDetector::detectObjects(const Mat& frame, // Frame
                                       vector<float>& confidences, // detection confidence
                                       vector<Rect>& boxes) // List of boxes created by YOLO model
 {
-    // Variables needed for the calculation
+    // Variables needed for the calculation frame size
     int imgWidth = frame.cols;
     int imgHeight = frame.rows;
 
-    Mat blob;
+    // Temp timers
+    // int64 start = cv::getTickCount();
+    
+    // Convert the frame into a tensor which is the format the neural networks expect
+    // Resize input frame to YOLO input size
+    cv::Mat resized;
 
-    // Convert the frame in blob which is the format the neural networks expect
-    blobFromImage(
-        frame, // Provided frame
-        blob, // Image to be processed by the model
-        1.0 / 255.0, // Scale factor this is the most common
-        Size(INPUT_WIDTH, INPUT_HEIGHT), // Image size
-        Scalar(),// mean substractions
-        true,// swap green to blue as YOLO use a different order for color channels
-        false// Keep the image info intact, do not crop just resize it.
+    cv::resize(
+        frame,
+        resized,
+        cv::Size(INPUT_WIDTH, INPUT_HEIGHT)
     );
 
-    net.setInput(blob); // pass the blob to the neural network
+    // Convert BGR to RGB
+    cv::cvtColor(
+        resized,
+        resized,
+        cv::COLOR_BGR2RGB
+    );
 
-    vector<Mat> outputs; // use a vector to store outputs
-    // the net passed its results
-    net.forward(outputs, net.getUnconnectedOutLayersNames());
+    // Prepare tensor data in CHW format
+    std::vector<float> inputTensorValues(
+        3 * INPUT_WIDTH * INPUT_HEIGHT
+    );
 
-    // Received the results, format them and stored
-    Mat output = outputs[0];
+    int channelSize =
+        INPUT_WIDTH * INPUT_HEIGHT;
 
+
+    for (int y = 0; y < INPUT_HEIGHT; y++)
+    {
+        for (int x = 0; x < INPUT_WIDTH; x++)
+        {
+            cv::Vec3b pixel =
+                resized.at<cv::Vec3b>(y, x);
+
+            // Red channel
+            inputTensorValues[
+                0 * channelSize +
+                y * INPUT_WIDTH +
+                x
+            ] = pixel[0] / 255.0f;
+
+            // Green channel
+            inputTensorValues[
+                1 * channelSize +
+                y * INPUT_WIDTH +
+                x
+            ] = pixel[1] / 255.0f;
+
+            // Blue channel
+            inputTensorValues[
+                2 * channelSize +
+                y * INPUT_WIDTH +
+                x
+            ] = pixel[2] / 255.0f;
+        }
+    }
+
+    std::array<int64_t,4> inputShape =
+    {
+        1,
+        3,
+        INPUT_HEIGHT,
+        INPUT_WIDTH
+    };
+    
+    Ort::Value inputTensor =
+        Ort::Value::CreateTensor<float>(
+            memoryInfo,
+            inputTensorValues.data(),
+            inputTensorValues.size(),
+            inputShape.data(),
+            inputShape.size());
+
+    // Timer
+    //int64 inferenceStart = cv::getTickCount();
+
+   // Pass the tnesor to the neural network
+    auto outputTensors =
+        session->Run(
+            Ort::RunOptions{nullptr},
+            inputNames.data(),
+            &inputTensor,
+            1,
+            outputNames.data(),
+            1);
+
+    // Timer
+    /*double inferenceTime =
+        (cv::getTickCount() - inferenceStart) /
+        cv::getTickFrequency();
+
+    std::cout
+        << "Inference: "
+        << inferenceTime * 1000
+        << " ms\n";*/
+    
+    float* outputData =
+        outputTensors[0].GetTensorMutableData<float>();
+
+    // Convert the output to a cv::Mat
+    auto info =
+        outputTensors[0].GetTensorTypeAndShapeInfo();
+
+    std::vector<int64_t> shape =
+        info.GetShape();
+    
+    // Wrap the ONNX buffer as an OpenCV matrix.
+    int dims[] =
+    {
+        (int)shape[0],
+        (int)shape[1],
+        (int)shape[2]
+    };
+
+    Mat output(
+        3,
+        dims,
+        CV_32F,
+        outputData);
+    
     if (output.dims == 3)
     {
         int rows = output.size[1];
@@ -121,7 +330,7 @@ void YOLOVideoDetector::detectObjects(const Mat& frame, // Frame
             transpose(output, output);
     }
 
-    // Preparing how the outputs will be received in the main loop
+    // Preparing how the boxes will be located in the frame
     float xFactor = static_cast<float>(imgWidth) / INPUT_WIDTH;
     float yFactor = static_cast<float>(imgHeight) / INPUT_HEIGHT;
 
@@ -135,21 +344,22 @@ void YOLOVideoDetector::detectObjects(const Mat& frame, // Frame
         float w = data[2];
         float h = data[3];
 
-        Mat scores(
-            1,
-            output.cols - 4,
-            CV_32FC1,
-            data + 4
-        );
+        // Simpler loop thank use minMaxLot
+        int bestClass = -1;
+        float bestScore = 0.0f;
 
-        Point classIdPoint;
-        double score;
-
-        minMaxLoc(scores, nullptr, &score, nullptr, &classIdPoint);
+        for (int c = 4; c < output.cols; ++c)
+        {
+            if (data[c] > bestScore)
+            {
+                bestScore = data[c];
+                bestClass = c - 4;
+            }
+        }
 
         // Pass only detected objects that meet the threshold
         // We can apply a stronger filter by increasing the threshold
-        if (score < SCORE_THRESHOLD)
+        if (bestScore < SCORE_THRESHOLD)
             continue;
 
         float left = (cx - 0.5f * w) * xFactor;
@@ -157,6 +367,7 @@ void YOLOVideoDetector::detectObjects(const Mat& frame, // Frame
         float width = w * xFactor;
         float height = h * yFactor;
 
+        // Compute bounding box
         boxes.emplace_back(
             Rect(
                 static_cast<int>(left),
@@ -167,8 +378,8 @@ void YOLOVideoDetector::detectObjects(const Mat& frame, // Frame
         );
 
         // Pass the class id and the confidence score for each of the detected objects
-        classIds.push_back(classIdPoint.x);
-        confidences.push_back(static_cast<float>(score));
+        classIds.push_back(bestClass);
+        confidences.push_back(bestScore);
     }
 }
 
@@ -287,4 +498,32 @@ string YOLOVideoDetector::getClassName(int classId) const
     }
 
     return to_string(classId);
+}
+
+// Method to write the detection log
+void YOLOVideoDetector::logDetection(int frameNumber,
+                                     const std::string& className,
+                                     float confidence,
+                                     const cv::Rect& box,
+                                     const cv::Size& frameSize)
+{
+    if (!logFile.is_open())
+        return;
+
+    // Calculate the box center
+    float centerX = box.x + box.width * 0.5f;
+    float centerY = box.y + box.height * 0.5f;
+
+    // format the info for the record
+    logFile << frameNumber << ","
+            << className << ","
+            << std::fixed << std::setprecision(3) << confidence << ","
+            << centerX << ","
+            << centerY << ","
+            << box.x << ","
+            << box.y << ","
+            << box.width << ","
+            << box.height << ","
+            << frameSize.width << ","
+            << frameSize.height << "\n";
 }
--- src/Object-Detection/YOLOVideoDetector.h
+++ src/Object-Detection/YOLOVideoDetector.h
@@ -4,6 +4,7 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
+#include <onnxruntime_cxx_api.h>
 
 #include <fstream>
 #include <string>
@@ -16,19 +17,35 @@ public:
     YOLOVideoDetector(const std::string& modelPath,
                       const std::string& classFilePath);
 
-    cv::Mat process(cv::Mat& frame) override;
+    cv::Mat process(cv::Mat &frame) override;
     std::string get_feature_name() override;
     
+    ~YOLOVideoDetector();
+    
 private:
-    static constexpr float SCORE_THRESHOLD = 0.25f;
-    static constexpr float CONFIDENCE_THRESHOLD = 0.25f;
-    static constexpr float NMS_THRESHOLD = 0.45f;
+    static constexpr float SCORE_THRESHOLD = 0.35f;//0.25f;
+    static constexpr float CONFIDENCE_THRESHOLD = 0.50f;//0.40f;
+    static constexpr float NMS_THRESHOLD = 0.45f;//0.45f;
 
     static constexpr int INPUT_WIDTH = 640;
     static constexpr int INPUT_HEIGHT = 640;
 
-    cv::dnn::Net net;
+    // ONNX Runtime
+    Ort::Env env;
+    Ort::SessionOptions sessionOptions;
+    std::unique_ptr<Ort::Session> session;
+    Ort::MemoryInfo memoryInfo;
+
+    // Cached model input/output names
+    std::vector<std::string> inputNameStorage;
+    std::vector<std::string> outputNameStorage;
+
+    std::vector<const char*> inputNames;
+    std::vector<const char*> outputNames;
     std::vector<std::string> classNames;
+    
+    std::ofstream logFile;
+    int frameNumber = 0;
 
     void loadClasses(const std::string& filename);
     void loadModel(const std::string& modelPath);
@@ -46,6 +63,12 @@ private:
     void drawFPS(cv::Mat& frame, int64& previousTick);
 
     std::string getClassName(int classId) const;
+    
+    void logDetection(int frameNumber,
+                      const std::string& className,
+                      float confidence,
+                      const cv::Rect& box,
+                      const cv::Size& frameSize);
 };
 
 #endif
